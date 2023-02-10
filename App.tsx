b3a862a3d9972843bb56d3aef90dc1e78b0f6dff
Okan <114703201+okanpehlivann@users.noreply.github.com>
2023-02-10 17:59:33 +0300

    feat: icon lib & home nav options added

--- App.tsx
+++ App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import {store} from './src/redux/store/store';
 import Home from './src/screens/Home/Home';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 function App(): JSX.Element {
   return (
     <Provider store={store}>
-      <Home />
+      <SafeAreaProvider>
+        <Home />
+      </SafeAreaProvider>
     </Provider>
   );
 }
--- src/components/NavOptions.tsx
+++ src/components/NavOptions.tsx
@@ -1,20 +1,37 @@
 import {
   FlatList,
+  Image,
   ListRenderItemInfo,
   Text,
   TouchableOpacity,
+  View,
 } from 'react-native';
 import React, {FC} from 'react';
 import {TNavOptionProps, TNavOptionsData} from '../interfaces/NavOptionsData';
+import tw from 'tailwind-react-native-classnames';
+import {Icon} from 'react-native-elements';
 
 const NavOptions: FC<TNavOptionProps> = props => {
   return (
     <FlatList
       data={props.navOptionsData}
       horizontal
+      keyExtractor={(item: TNavOptionsData) => item.id}
       renderItem={({item}: ListRenderItemInfo<TNavOptionsData>) => (
-        <TouchableOpacity>
-          <Text>{item.title}</Text>
+        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+          <View>
+            <Image
+              style={{width: 120, height: 120, resizeMode: 'contain'}}
+              source={{uri: item.image}}
+            />
+            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+            <Icon
+              style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+              name="arrowright"
+              color="white"
+              type="antdesign"
+            />
+          </View>
         </TouchableOpacity>
       )}
     />
